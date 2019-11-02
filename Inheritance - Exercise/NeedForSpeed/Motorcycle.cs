d2fc14af2a5aa5ea1067fbd4995bc191d810b9ff
drapperr <drapperr@users.noreply.github.com>
2019-11-02 18:09:49 +0200

    update

---
+++ Inheritance - Exercise/NeedForSpeed/Motorcycle.cs
@@ -0,0 +1,10 @@
+﻿namespace NeedForSpeed
+{
+    public class Motorcycle : Vehichle
+    {
+        public Motorcycle(int horsePower, double fuel) 
+            : base(horsePower, fuel)
+        {
+        }
+    }
+}
