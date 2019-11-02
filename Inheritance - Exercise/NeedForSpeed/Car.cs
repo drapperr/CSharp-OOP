112daa4b570b36b418d8cf753924c1bd47c4e03d
drapperr <drapperr@users.noreply.github.com>
2019-11-02 18:09:22 +0200

    update

---
+++ Inheritance - Exercise/NeedForSpeed/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace NeedForSpeed
+{
+    public class Car : Vehichle
+    {
+        private const double DefaultFuelConsumption = 3.0;
+
+        public Car(int horsePower, double fuel) 
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => DefaultFuelConsumption;
+    }
+}
