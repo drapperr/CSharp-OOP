3bfc0801f763e351da8bf71a54d76e3a60d149bd
drapperr <drapperr@users.noreply.github.com>
2019-11-03 19:16:01 +0200

    update

---
+++ Working with Abstraction - Exercise/P02_CarsSalesman/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_CarsSalesman
+{
+    public class CarSalesman
+    {
+        public static void Main()
+        {
+            Runner.Run();
+        }
+    }
+}
---
+++ Working with Abstraction - Exercise/P03_JediGalaxy/Galaxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_JediGalaxy
+{
+    public class Galaxy
+    {
+        public Galaxy(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+            this.matrix = new int[row, col];
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public int[,] matrix { get; set; }
+    }
+}
