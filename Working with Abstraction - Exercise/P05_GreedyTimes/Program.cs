7845e63ff7c0e728a48478017b3d54863901b4c1
drapperr <drapperr@users.noreply.github.com>
2019-11-03 19:16:58 +0200

    update

---
+++ Working with Abstraction - Exercise/P05_GreedyTimes/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+    public class Potato
+    {
+        static void Main(string[] args)
+        {
+            long capacity = long.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+            long gold = 0;
+            long stone = 0;
+            long money = 0;
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                string name = input[i];
+                long count = long.Parse(input[i + 1]);
+
+                string item = string.Empty;
+
+                if (name.Length == 3)
+                {
+                    item = "Cash";
+                }
+                else if (name.ToLower().EndsWith("gem"))
+                {
+                    item = "Gem";
+                }
+                else if (name.ToLower() == "gold")
+                {
+                    item = "Gold";
+                }
+
+                if (item == "")
+                {
+                    continue;
+                }
+                else if (capacity < bag.Values.Select(x => x.Values.Sum()).Sum() + count)
+                {
+                    continue;
+                }
+
+                switch (item)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(item))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (count > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[item].Values.Sum() + count > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Cash":
+                        if (!bag.ContainsKey(item))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (count > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[item].Values.Sum() + count > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(item))
+                {
+                    bag[item] = new Dictionary<string, long>();
+                }
+
+                if (!bag[item].ContainsKey(name))
+                {
+                    bag[item][name] = 0;
+                }
+
+                bag[item][name] += count;
+                if (item == "Gold")
+                {
+                    gold += count;
+                }
+                else if (item == "Gem")
+                {
+                    stone += count;
+                }
+                else if (item == "Cash")
+                {
+                    money += count;
+                }
+            }
+
+            foreach (var x in bag)
+            {
+                Console.WriteLine($"<{x.Key}> ${x.Value.Values.Sum()}");
+                foreach (var item2 in x.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{item2.Key} - {item2.Value}");
+                }
+            }
+        }
+    }
+}
