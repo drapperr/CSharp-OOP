df90961233236e7b0b089e10d629b4d9a6fcb2b5
drapperr <drapperr@users.noreply.github.com>
2019-11-04 18:55:56 +0200

    update

---
+++ Working with Abstraction - Lab/Point in Rectangle/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace PointInRectangle
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //"{topLeftX} {topLeftY} {bottomRightX} {bottomRightY}"
+            int[] rectanglePoints = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int topLeftX = rectanglePoints[0];
+            int topLeftY = rectanglePoints[1];
+            int bottomRightX = rectanglePoints[2];
+            int bottomRightY = rectanglePoints[3];
+
+            Point topLeft = new Point(topLeftX, topLeftY);
+            Point bottomRight = new Point(bottomRightX, bottomRightY);
+
+            Rectangle rectangle = new Rectangle(topLeft, bottomRight);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] pointCoordinates = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+                int x = pointCoordinates[0];
+                int y = pointCoordinates[1];
+
+                Point point = new Point(x, y);
+
+                Console.WriteLine(rectangle.Contains(point));
+            }
+        }
+    }
+}
