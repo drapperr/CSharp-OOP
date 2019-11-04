faaeb2ba99e855047f6a18a39dfeef182722de68
drapperr <drapperr@users.noreply.github.com>
2019-11-04 18:56:39 +0200

    update

---
+++ Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+        public Coffee(string name, decimal price= CoffeePrice, double milliliters= CoffeeMilliliters) 
+            : base(name, price, milliliters)
+        {
+        }
+
+        public double Caffeine  { get; set; }
+    }
+}
