e658c47c0dd7d4164412aa0ec84149f4bf1ffa2b
drapperr <drapperr@users.noreply.github.com>
2019-11-04 18:55:38 +0200

    update

---
+++ Working with Abstraction - Lab/Point in Rectangle/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get;set; }
+        public Point BottomRight { get;set; }
+
+        public bool Contains(Point point)
+        {
+            bool insideX = point.X >= TopLeft.X && point.X <= BottomRight.X;
+            bool insideY = point.Y >= TopLeft.Y && point.Y <= BottomRight.Y;
+
+            return insideX && insideY;
+        }
+    }
+}
