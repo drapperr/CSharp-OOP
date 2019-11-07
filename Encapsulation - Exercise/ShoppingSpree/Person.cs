e30b88503dd9cce059f4ff0984ceb515ba7026fc
drapperr <drapperr@users.noreply.github.com>
2019-11-07 16:00:44 +0200

    update

---
+++ Encapsulation - Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person : IEnumerable<string>
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> BagOfProducts => bagOfProducts.AsReadOnly();
+
+        public void BuyProduct(Product product)
+        {
+            if (product.Cost > this.Money)
+            {
+                Console.WriteLine($"{this.name} can't afford {product.Name}");
+            }
+            else
+            {
+                this.money -= product.Cost;
+                bagOfProducts.Add(product);
+                Console.WriteLine($"{this.name} bought {product.Name}");
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var product in this.bagOfProducts)
+            {
+                yield return product.Name;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Encapsulation - Exercise/ShoppingSpree/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] peopleInput = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            string[] productsInput = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string,Person> people = new Dictionary<string,Person>();
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            try
+            {
+                foreach (var person in peopleInput)
+                {
+                    string[] personArgs = person.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string name = personArgs[0];
+                    decimal money = decimal.Parse(personArgs[1]);
+
+                    if (!people.ContainsKey(name))
+                    {
+                        people.Add(name, new Person(name, money));
+                    }
+                }
+
+                foreach (var product in productsInput)
+                {
+                    string[] productArgs = product.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string name = productArgs[0];
+                    decimal cost = decimal.Parse(productArgs[1]);
+
+                    if (!products.ContainsKey(name))
+                    {
+                        products.Add(name, new Product(name, cost));
+                    }
+                }
+                string input = string.Empty;
+
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] inputArgs = input.Split(' ');
+                    string personName = inputArgs[0];
+                    string productName = inputArgs[1];
+
+                    people[personName].BuyProduct(products[productName]);
+                }
+
+                foreach (var (name, person) in people)
+                {
+                    Console.Write($"{name} - ");
+
+                    if (person.BagOfProducts.Count == 0)
+                    {
+                        Console.WriteLine("Nothing bought");
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Join(", ", person));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
