a785f1d292de21b117485c4c46b116ec7460dcd1
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-12 18:22:32 +0200

    update

---
+++ Interfaces and Abstraction - Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class Smartphone:ICallable,IBrowseble
+    {
+        public Smartphone(string model)
+        {
+            this.Model = model;
+        }
+
+        public string Model { get; set; }
+
+        public void Browse(string site)
+        {
+            foreach (var ch in site)
+            {
+                if (char.IsDigit(ch))
+                {
+                    Console.WriteLine("Invalid URL!");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Browsing: {site}!");
+        }
+
+        public void Call(string number)
+        {
+            foreach (var ch in number)
+            {
+                if (!char.IsDigit(ch))
+                {
+                    Console.WriteLine("Invalid number!");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Calling... {number}");
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/Telephony/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] sites = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Smartphone smartphone = new Smartphone("Samsung");
+
+            foreach (var number in numbers)
+            {
+                smartphone.Call(number);
+            }
+
+            foreach (var site in sites)
+            {
+                smartphone.Browse(site);
+            }
+        }
+    }
+}
