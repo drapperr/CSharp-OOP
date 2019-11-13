5ccd008c832607f271b47838730b043575b33a64
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-13 14:14:07 +0200

    update

---
+++ Interfaces and Abstraction - Exercise/CollectionHierarchy/IAddCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionHierarchy
+{
+    public interface IAddCollection
+    {
+        List<string> Items { get; }
+
+        int Add(string item);
+    }
+}
