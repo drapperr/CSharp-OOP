2270e3116f3aaca9a7fe9cad51d6571a13576a78
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-13 14:15:07 +0200

    update

---
+++ Interfaces and Abstraction - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 0; i < this.height-2; i++)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.WriteLine(end + new string(mid, width - 2) + end);
+        }
+    }
+}
