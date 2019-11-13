3fd86716c07e10725790e5e01a69acd5a08f5bec
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-13 14:14:25 +0200

    update

---
+++ Interfaces and Abstraction - Exercise/CollectionHierarchy/MyList.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionHierarchy
+{
+    public class MyList : AddRemoveCollection, IMyList
+    {
+        public override string Remove()
+        {
+            string item = Items[0];
+            Items.RemoveAt(0);
+            return item;
+        }
+
+        public int Used => Items.Count;
+    }
+}
