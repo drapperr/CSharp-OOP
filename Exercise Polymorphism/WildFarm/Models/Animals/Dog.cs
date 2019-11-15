8dc8057f5ef8802ee53cac80d384109213709875
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-16 01:04:16 +0200

    update

---
+++ Exercise Polymorphism/WildFarm/Models/Animals/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Models.Animals
+{
+    public class Dog : Mammal
+    {
+        public Dog(string name, double weight, string livingRegion) 
+            : base(name, weight, livingRegion)
+        {
+            IncreasedConst = 0.40;
+        }
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+
+        public override List<Type> AllowedFoods => new List<Type>()
+        {
+            typeof(Meat),
+        };
+    }
+}
---
+++ Exercise Polymorphism/WildFarm/Models/Animals/Hen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Models.Animals
+{
+    public class Hen : Bird
+    {
+        public Hen(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+            IncreasedConst = 0.35;
+        }
+
+        public override string ProduceSound()
+        {
+            return "Cluck";
+        }
+
+        public override List<Type> AllowedFoods => new List<Type>()
+        {
+            typeof(Fruit),
+            typeof(Meat),
+            typeof(Seeds),
+            typeof(Vegetable),
+        };
+    }
+}
