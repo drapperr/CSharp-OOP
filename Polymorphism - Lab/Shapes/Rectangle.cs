4a99d827b5ae48ae85c3782bb98fd99842500ac4
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-15 21:04:18 +0200

    update

---
+++ Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * width + 2 * height;
+        }
+
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
