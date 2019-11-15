0621d8b5ab8e6ae0f341303c88346a3c244169b2
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-15 21:03:11 +0200

    update

---
+++ Exercise Polymorphism/Vehicles/Truck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+
+        private const double acConsumption = 1.6;
+
+        public Truck(double fuel, double fuelConsumption, double tankCapacity)
+            : base(fuel, fuelConsumption, tankCapacity)
+        {
+            fuelConsumption += acConsumption;
+        }
+
+        public override void Refuel(double liters)
+        {
+            if (liters <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+            if (Fuel + liters > TankCapacity)
+            {
+                throw new ArgumentException($"Cannot fit {liters} fuel in the tank");
+            }
+            Fuel += liters * 0.95;
+        }
+    }
+}
---
+++ Exercise Polymorphism/Vehicles/Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        protected Vehicle(double fuel, double fuelConsumption, double tankCapacity)
+        {
+            if (fuel > tankCapacity)
+            {
+                fuel = 0;
+            }
+            Fuel = fuel;
+            FuelConsumption = fuelConsumption;
+            TankCapacity = tankCapacity;
+        }
+        public double Fuel { get; protected set; }
+
+        public double FuelConsumption { get; protected set; }
+
+        public double AcFuelConsumption { get; protected set; }
+
+        public double TankCapacity { get; protected set; }
+
+        public virtual void Drive(double distance)
+        {
+            if (Fuel - distance * FuelConsumption >= 0)
+            {
+                Fuel -= distance * FuelConsumption;
+                Console.WriteLine($"{this.GetType().Name} travelled {distance} km");
+                return;
+            }
+            Console.WriteLine($"{this.GetType().Name} needs refueling");
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            if (liters <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+            if (Fuel + liters > TankCapacity)
+            {
+                throw new ArgumentException($"Cannot fit {liters} fuel in the tank");
+            }
+            Fuel += liters;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {Fuel:F2}";
+        }
+    }
+}
