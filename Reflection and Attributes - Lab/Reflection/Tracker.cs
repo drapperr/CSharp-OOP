736f4b6183921e5eb8f5141390db3c101433b094
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-21 12:18:57 +0200

    update

---
+++ Reflection and Attributes - Lab/Reflection/Tracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var type = typeof(StartUp);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+        foreach (var methodInfo in methods)
+        {
+            if (methodInfo.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = methodInfo.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{methodInfo.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
+
