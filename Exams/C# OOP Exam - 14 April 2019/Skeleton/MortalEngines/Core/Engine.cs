24a2b356e76c5245288f1c891ef29d9de9f60d67
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-24 06:15:25 +0200

    update

---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Core/Engine.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Core.Contracts;
+
+namespace MortalEngines.Core
+{
+    public class Engine : IEngine
+    {
+        private IMachinesManager machinesManager;
+
+        public Engine()
+        {
+            machinesManager = new MachinesManager();
+        }
+        public void Run()
+        {
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                string[] inputArgs = input.Split(" ");
+                string command = inputArgs[0];
+
+                try
+                {
+                    string result = string.Empty;
+
+                    switch (command)
+                    {
+                        case "HirePilot":
+                            result = machinesManager.HirePilot(inputArgs[1]);
+                            break;
+
+                        case "PilotReport":
+                            result = machinesManager.PilotReport(inputArgs[1]);
+                            break;
+
+                        case "ManufactureTank":
+                            result = machinesManager.ManufactureTank(inputArgs[1],
+                                double.Parse(inputArgs[2]),
+                                double.Parse(inputArgs[3]));
+                            break;
+
+                        case "ManufactureFighter":
+                            result = machinesManager.ManufactureFighter(inputArgs[1],
+                                double.Parse(inputArgs[2]),
+                                double.Parse(inputArgs[3]));
+                            break;
+
+                        case "MachineReport":
+                            result = machinesManager.MachineReport(inputArgs[1]);
+                            break;
+
+                        case "AggressiveMode":
+                            result = machinesManager.ToggleFighterAggressiveMode(inputArgs[1]);
+                            break;
+
+                        case "DefenseMode":
+                            result = machinesManager.ToggleTankDefenseMode(inputArgs[1]);
+                            break;
+
+                        case "Engage":
+                            result = machinesManager.EngageMachine(inputArgs[1],
+                                inputArgs[2]);
+                            break;
+
+                        case "Attack":
+                            result=machinesManager.AttackMachines(inputArgs[1],
+                                inputArgs[2]);
+                            break;
+
+                        case "Quit":
+                            Environment.Exit(0);
+                            break;
+                    }
+
+                    Console.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error:" + e.Message);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Core/MachinesManager.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using MortalEngines.Entities;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Core
+{
+    using Contracts;
+
+    public class MachinesManager : IMachinesManager
+    {
+        private Dictionary<string, IPilot> pilots;
+        private Dictionary<string, IMachine> machines;
+
+        public MachinesManager()
+        {
+            pilots = new Dictionary<string, IPilot>();
+            machines = new Dictionary<string, IMachine>();
+        }
+
+        public string HirePilot(string name)
+        {
+            if (pilots.ContainsKey(name))
+            {
+                return $"Pilot {name} is hired already";
+            }
+            else
+            {
+                pilots.Add(name, new Pilot(name));
+                return $"Pilot {name} hired";
+            }
+        }
+
+        public string PilotReport(string pilotReporting)
+        {
+            if (!pilots.ContainsKey(pilotReporting))
+            {
+                return $"Pilot {pilotReporting} could not be found";
+            }
+
+            return pilots[pilotReporting].Report();
+        }
+
+        public string MachineReport(string machineName)
+        {
+            if (!machines.ContainsKey(machineName))
+            {
+                return $"Machine {machineName} could not be found";
+            }
+
+            return machines[machineName].ToString();
+        }
+
+        public string ManufactureTank(string name, double attackPoints, double defensePoints)
+        {
+            if (machines.ContainsKey(name))
+            {
+                return $"Machine {name} is manufactured already";
+            }
+            Tank tank=new Tank(name, attackPoints, defensePoints);
+            machines.Add(name,tank);
+
+            return $"Tank {name} manufactured - attack: {tank.AttackPoints:F2}; defense: {tank.DefensePoints:F2}";
+        }
+        public string ToggleTankDefenseMode(string tankName)
+        {
+            if (!machines.ContainsKey(tankName))
+            {
+                return $"Machine {tankName} could not be found";
+            }
+
+            if (machines[tankName] is Tank)
+            {
+                (machines[tankName] as Tank)?.ToggleDefenseMode();            //????
+                return $"Tank {tankName} toggled defense mode";
+            }
+
+            return $"Machine {tankName} could not be found";
+        }
+
+        public string ManufactureFighter(string name, double attackPoints, double defensePoints)
+        {
+            if (machines.ContainsKey(name))
+            {
+                return $"Machine {name} is manufactured already";
+            }
+            Fighter fighter=new Fighter(name, attackPoints, defensePoints);
+            machines.Add(name,fighter);
+
+            return $"Fighter {name} manufactured - attack: {fighter.AttackPoints:F2}; defense: {fighter.DefensePoints:F2}; aggressive: ON";
+        }
+
+        public string ToggleFighterAggressiveMode(string fighterName)
+        {
+            if (!machines.ContainsKey(fighterName))
+            {
+                return $"Machine {fighterName} could not be found";
+            }
+
+            if (machines[fighterName] is Fighter)
+            {
+                (machines[fighterName] as Fighter)?.ToggleAggressiveMode();         //??????
+                return $"Fighter {fighterName} toggled aggressive mode";
+            }
+
+            return $"Machine {fighterName} could not be found";
+        }
+
+        public string EngageMachine(string selectedPilotName, string selectedMachineName)
+        {
+            if (!pilots.ContainsKey(selectedPilotName))
+            {
+                return $"Pilot {selectedPilotName} could not be found";
+            }
+
+            if (!machines.ContainsKey(selectedMachineName))
+            {
+                return $"Machine {selectedMachineName} could not be found";
+            }
+
+            if (machines[selectedMachineName].Pilot != null)
+            {
+                return $"Machine {selectedMachineName} is already occupied";
+            }
+
+            machines[selectedMachineName].Pilot = pilots[selectedPilotName];
+
+            pilots[selectedPilotName].AddMachine(machines[selectedMachineName]);
+
+            return $"Pilot {selectedPilotName} engaged machine {selectedMachineName}";
+        }
+
+        public string AttackMachines(string attackingMachineName, string defendingMachineName)
+        {
+            if (!machines.ContainsKey(attackingMachineName))
+            {
+                return $"Machine {attackingMachineName} could not be found";
+            }
+
+            if (!machines.ContainsKey(defendingMachineName))
+            {
+                return $"Machine {defendingMachineName} could not be found";
+            }
+
+            if (machines[defendingMachineName].HealthPoints == 0)
+            {
+                return $"Dead machine {defendingMachineName} cannot attack or be attacked";
+            }
+
+            if (machines[attackingMachineName].HealthPoints==0)
+            {
+                return $"Dead machine {attackingMachineName} cannot attack or be attacked";
+            }
+            
+            double attackPoints = machines[attackingMachineName].AttackPoints;
+
+            attackPoints -= machines[defendingMachineName].DefensePoints;
+
+            if (attackPoints > 0)
+            {
+                machines[defendingMachineName].HealthPoints -= attackPoints;
+            }
+            else
+            {
+                attackPoints = 0;
+            }
+
+            if (machines[defendingMachineName].HealthPoints <= 0)
+            {
+                machines[defendingMachineName].HealthPoints = 0;
+            }
+
+            if (!machines[attackingMachineName].Targets.Contains(machines[defendingMachineName].Name))
+            {
+                machines[attackingMachineName].Targets.Add(machines[defendingMachineName].Name);
+            }
+           
+            return
+                $"Machine {defendingMachineName} was attacked by machine {attackingMachineName} - current health: {machines[defendingMachineName].HealthPoints:F2}";
+        }
+    }
+}
