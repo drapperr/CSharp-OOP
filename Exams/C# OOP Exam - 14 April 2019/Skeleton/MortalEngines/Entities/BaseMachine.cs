8fba2aaddecdebd6ff40482ab41d23a7ed849bb7
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-24 06:15:48 +0200

    update

---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Entities/BaseMachine.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public abstract class BaseMachine : IMachine
+    {
+        private string name;
+        private IPilot pilot;
+
+        protected BaseMachine(string name, double attackPoints, double defensePoints, double healthPoints)
+        {
+            this.Name = name;
+            this.AttackPoints = attackPoints;
+            this.DefensePoints = defensePoints;
+            this.HealthPoints = healthPoints;
+            this.Targets = new List<string>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    this.name = value;
+                }
+
+                this.name = value;
+            }
+        }
+
+        public IPilot Pilot
+        {
+            get => this.pilot;
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException("Pilot cannot be null.");
+                }
+
+                pilot = value;
+            }
+        }
+
+        public double HealthPoints { get; set; }
+
+        public double AttackPoints { get; protected set; }
+
+        public double DefensePoints { get; protected set; }
+
+        public IList<string> Targets { get; }
+
+        public void Attack(IMachine target)
+        {
+            if (target==null)
+            {
+                throw new NullReferenceException("Target cannot be null");
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb=new StringBuilder();
+            sb.AppendLine($"- {this.name}");
+            sb.AppendLine($" *Type: {this.GetType().Name}");
+            sb.AppendLine($" *Health: {this.HealthPoints:F2}");
+            sb.AppendLine($" *Attack: {this.AttackPoints:F2}");
+            sb.AppendLine($" *Defense: {this.DefensePoints:F2}");
+            sb.Append(" *Targets: ");
+
+            if (this.Targets.Count==0)
+            {
+                sb.AppendLine("None");
+            }
+            else
+            {
+                sb.AppendLine(string.Join(",",this.Targets));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
