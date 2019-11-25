bde6d6bf51aa900485d7a4c4a643514e42ec0ff8
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-25 09:31:37 +0200

    update

---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Entities/Pilot.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public class Pilot : IPilot
+    {
+        private string name;
+        private readonly List<IMachine> machines;
+
+        public Pilot(string name)
+        {
+            this.Name = name;
+            this.machines=new List<IMachine>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Pilot name cannot be null or empty string.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public void AddMachine(IMachine machine)
+        {
+            if (machine==null)
+            {
+                throw new NullReferenceException("Null machine cannot be added to the pilot.");
+            }
+
+            machines.Add(machine);
+        }
+
+        public string Report()
+        {
+            var sb=new StringBuilder();
+            sb.AppendLine($"{this.Name} - {this.machines.Count} machines");
+
+            foreach (IMachine machine in machines)
+            {
+                sb.AppendLine($"- {machine.Name}");
+                sb.AppendLine($" *Type: {machine.GetType().Name}");
+                sb.AppendLine($" *Health: {machine.HealthPoints:F2}");
+                sb.AppendLine($" *Attack: {machine.AttackPoints:F2}");
+                sb.AppendLine($" *Defense: {machine.DefensePoints:F2}");
+                sb.AppendLine($" *Targets: {machine.Targets.Count}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Entities/Tank.cs
@@ -0,0 +1,47 @@
+﻿using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public class Tank : BaseMachine, ITank
+    {
+        private const double initialHealthPoints = 100;
+        private bool defenseMode;
+
+        public Tank(string name, double attackPoints, double defensePoints)
+            : base(name, attackPoints, defensePoints, initialHealthPoints)
+        {
+            this.defenseMode = false;
+            this.ToggleDefenseMode();
+        }
+
+        public bool DefenseMode => this.defenseMode;
+
+        public void ToggleDefenseMode()
+        {
+            this.defenseMode = !this.defenseMode;
+
+            if (defenseMode)
+            {
+                this.AttackPoints -= 40;
+                this.DefensePoints += 30;
+            }
+            else
+            {
+                this.AttackPoints += 40;
+                this.DefensePoints -= 30;
+            }
+        }
+
+        public override string ToString()
+        {
+            string mode = "OFF";
+
+            if (defenseMode)
+            {
+                mode = "ON";
+            }
+
+            return base.ToString() + $" *Defense: {mode}";
+        }
+    }
+}
