ed76f9911cc8a4d9f92ff1b40df7b96e1d5a839d
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-25 09:31:10 +0200

    update

---
+++ Exams/C# OOP Exam - 14 April 2019/Skeleton/MortalEngines/Entities/Fighter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Entities
+{
+    public class Fighter : BaseMachine,IFighter
+    {
+        private const double initialHealthPoints = 200;
+        private bool aggressiveMode;
+
+        public Fighter(string name, double attackPoints, double defensePoints)
+            : base(name, attackPoints, defensePoints, initialHealthPoints)
+        {
+            this.aggressiveMode = false;   
+            this.ToggleAggressiveMode();
+        }
+
+        public bool AggressiveMode => this.aggressiveMode;
+
+        public void ToggleAggressiveMode()
+        {
+            this.aggressiveMode = !this.aggressiveMode;
+
+            if (aggressiveMode)
+            {
+                this.AttackPoints += 50;
+                this.DefensePoints -= 25;
+            }
+            else
+            {
+                this.AttackPoints -= 50;
+                this.DefensePoints += 25;
+            }
+        }
+
+        public override string ToString()
+        {
+            string mode = "OFF";
+            if (aggressiveMode)
+            {
+                mode = "ON";
+            }
+            return base.ToString() + $" *Aggressive: {mode}";
+        }
+    }
+}
