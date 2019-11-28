278588480140eaa093416489ab69247cbc03a80f
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-28 22:22:37 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Models/BattleFields/BattleField.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayersAndMonsters.Models.BattleFields.Contracts;
+using PlayersAndMonsters.Models.Cards.Contracts;
+using PlayersAndMonsters.Models.Players;
+using PlayersAndMonsters.Models.Players.Contracts;
+
+namespace PlayersAndMonsters.Models.BattleFields
+{
+    public class BattleField : IBattleField
+    {
+        public void Fight(IPlayer attackPlayer, IPlayer enemyPlayer)
+        {
+            if (attackPlayer.IsDead||enemyPlayer.IsDead)
+            {
+               throw new ArgumentException("Player is dead!");
+            }
+
+            if (attackPlayer is Beginner)
+            {
+                attackPlayer.Health += 40;
+
+                foreach (var card in attackPlayer.CardRepository.Cards)
+                {
+                    card.DamagePoints += 30;
+                }
+            }
+
+            if (enemyPlayer is Beginner)
+            {
+                enemyPlayer.Health += 40;
+
+                foreach (var card in enemyPlayer.CardRepository.Cards)
+                {
+                    card.DamagePoints += 30;
+                }
+            }
+
+            attackPlayer.Health += attackPlayer.CardRepository.Cards.Sum(x => x.DamagePoints);
+            enemyPlayer.Health += enemyPlayer.CardRepository.Cards.Sum(x => x.DamagePoints);
+
+            int attackPlayerDamage = attackPlayer.CardRepository.Cards.Sum(x => x.DamagePoints);
+            int enemyPlayerDamage = enemyPlayer.CardRepository.Cards.Sum(x => x.DamagePoints);
+
+            while (true)
+            {
+                enemyPlayer.TakeDamage(attackPlayerDamage);
+                if (enemyPlayer.IsDead)
+                {
+                    break;
+                }
+
+                attackPlayer.TakeDamage(enemyPlayerDamage);
+                if (attackPlayer.IsDead)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
