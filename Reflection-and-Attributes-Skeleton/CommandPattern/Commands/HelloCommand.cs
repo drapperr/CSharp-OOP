c65f1f32d6615073296305cbb31f1b998b45f9c2
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-29 15:23:51 +0200

    update

---
+++ Reflection-and-Attributes-Skeleton/CommandPattern/Commands/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string name = args[0];
+
+            return $"Hello, {name}";
+        }
+    }
+}
