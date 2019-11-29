f73933134c7d60ccbea4f211d3eb06d85350c0f3
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-29 15:24:28 +0200

    update

---
+++ Reflection-and-Attributes-Skeleton/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+        public override bool IsValid(object obj)
+        {
+            if (obj is int valueAsInt)
+            {
+                if (valueAsInt >= this.minValue && valueAsInt <= this.maxValue)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            throw new ArgumentException("Invalid type");
+        }
+    }
+}
