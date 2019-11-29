a865aedb9e80771347592039e32e7071ea678805
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-29 15:24:08 +0200

    update

---
+++ Reflection-and-Attributes-Skeleton/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = (inputArgs[0]+"Command").ToLower();
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            Type commandType = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(n => n.Name.ToLower() == commandName);
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type");
+            }
+
+            var instanceType = Activator.CreateInstance(commandType) as ICommand;
+
+            if (instanceType == null)
+            {
+                throw new ArgumentException("Invalid command type");
+            }
+
+            string result = instanceType.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ Reflection-and-Attributes-Skeleton/CommandPattern/Core/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                string result = commandInterpreter.Read(input);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
