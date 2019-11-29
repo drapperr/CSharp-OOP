8adaf4f81958595577c648c87e00ef88cf5a62ee
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-29 15:25:20 +0200

    uupdate

---
+++ Reflection-and-Attributes-Skeleton/ValidationAttributes/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            var objProperties = obj.GetType()
+                .GetProperties();
+
+            foreach (var propertyInfo in objProperties)
+            {
+                var propAttributes = propertyInfo
+                    .GetCustomAttributes()
+                    .Where(x => x is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (var propAttribute in propAttributes)
+                {
+                    bool result = propAttribute.IsValid(propertyInfo.GetValue(obj));
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
