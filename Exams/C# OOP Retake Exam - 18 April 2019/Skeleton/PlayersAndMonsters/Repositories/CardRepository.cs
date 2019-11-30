62e57546b30e6a73fd9033a2df1935124207c8ab
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-30 18:29:46 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Repositories/CardRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayersAndMonsters.Models.Cards.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class CardRepository : ICardRepository
+    {
+        private List<ICard> cards;
+
+        public CardRepository()
+        {
+            this.cards = new List<ICard>();
+        }
+
+        public int Count => cards.Count;
+
+        public IReadOnlyCollection<ICard> Cards => this.cards.AsReadOnly();
+
+        public void Add(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+
+            if (cards.Any(c => c.Name == card.Name))
+            {
+                throw new ArgumentException($"Card {card.Name} already exists!");
+            }
+
+            cards.Add(card);
+        }
+
+        public bool Remove(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+
+            return cards.Remove(card);
+        }
+
+        public ICard Find(string name)
+        {
+            return cards.FirstOrDefault(c => c.Name == name);
+        }
+    }
+}
