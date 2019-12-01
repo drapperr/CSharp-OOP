1f897e43e1c9a37ffa7324c49d3bfac2e0b06792
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-01 14:01:38 +0200

    update

---
+++ Unit Testing - Exercises/Database.Tests/DatabaseTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DatabaseTests
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            Assert.Pass();
+        }
+    }
+}
