f1b69866415eb06828b012c6f03b5ae1af01c92d
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-01 14:00:51 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Repositories/PlayerRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public int Count => players.Count;
+
+        public IReadOnlyCollection<IPlayer> Players => this.players.AsReadOnly();
+
+        public void Add(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            if (this.players.Any(p => p.Username == player.Username))
+            {
+                throw new ArgumentException($"Player {player.Username} already exists!");
+            }
+
+            players.Add(player);
+        }
+
+        public bool Remove(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            return players.Remove(player);
+        }
+
+        public IPlayer Find(string username)
+        {
+            return players.FirstOrDefault(p => p.Username == username);
+        }
+    }
+}
