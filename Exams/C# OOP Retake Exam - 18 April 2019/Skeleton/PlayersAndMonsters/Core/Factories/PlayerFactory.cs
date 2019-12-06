3cc4e62a32428b82e691dd90b24a4df67a082b79
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-06 10:26:11 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Core/Factories/PlayerFactory.cs
@@ -0,0 +1,24 @@
+﻿using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Players;
+using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string type, string username)
+        {
+            if (type == nameof(Beginner))
+            {
+                return new Beginner(new CardRepository(), username);
+            }
+            else if (type == nameof(Advanced))
+            {
+                return new Advanced(new CardRepository(), username);
+            }
+
+            return null;
+        }
+    }
+}
