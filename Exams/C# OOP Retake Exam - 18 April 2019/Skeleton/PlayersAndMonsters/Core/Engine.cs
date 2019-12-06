6e1cfd0efb81a8a0922bd39eab757f6a1bda97fb
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-06 10:25:45 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Core/Engine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Core.Contracts
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            ManagerController managerController = new ManagerController();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                string command = input[0];
+
+                try
+                {
+                    if (command == "AddPlayer")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+
+                        string result = managerController.AddPlayer(type, name);
+                        Console.WriteLine(result);
+                    }
+                    else if (command == "AddCard")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+
+                        string result = managerController.AddCard(type, name);
+                        Console.WriteLine(result);
+                    }
+                    else if (command == "AddPlayerCard")
+                    {
+                        string userName = input[1];
+                        string cardName = input[2];
+
+                        string result = managerController.AddPlayerCard(userName, cardName);
+                        Console.WriteLine(result);
+                    }
+                    else if (command == "Fight")
+                    {
+                        string attacker = input[1];
+                        string enemy = input[2];
+
+                        string result = managerController.Fight(attacker, enemy);
+                        Console.WriteLine(result);
+                    }
+                    else if (command == "Report")
+                    {
+                        string result = managerController.Report();
+                        Console.WriteLine(result);
+                    }
+                    else if (command == "Exit")
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
