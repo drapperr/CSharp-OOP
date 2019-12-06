304d7d2c5e515d53cba98f57915a21cb1d6975d7
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-06 10:25:58 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 18 April 2019/Skeleton/PlayersAndMonsters/Core/Factories/CardFactory.cs
@@ -0,0 +1,23 @@
+﻿using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Cards;
+using PlayersAndMonsters.Models.Cards.Contracts;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class CardFactory : ICardFactory
+    {
+        public ICard CreateCard(string type, string name)
+        {
+            if (type + "Card" == nameof(MagicCard))
+            {
+                return new MagicCard(name);
+            }
+            else if (type + "Card" == nameof(TrapCard))
+            {
+                return new TrapCard(name);
+            }
+
+            return null;
+        }
+    }
+}
