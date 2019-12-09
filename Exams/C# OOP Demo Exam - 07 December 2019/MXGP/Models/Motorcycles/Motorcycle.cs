35e0b8004ddd6b5711ec814216474743a43f6dfa
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-09 11:29:23 +0200

    update

---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Models/Motorcycles/Motorcycle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MXGP.Models.Motorcycles.Contracts;
+
+namespace MXGP.Models.Motorcycles
+{
+    public abstract class Motorcycle : IMotorcycle
+    {
+        private string model;
+
+        protected Motorcycle(string model,int horsePower,double cubicCentimeters)
+        {
+            Model = model;
+            HorsePower = horsePower;
+            CubicCentimeters = cubicCentimeters;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 4)
+                {
+                    throw new ArgumentException($"Model {value} cannot be less than 4 symbols.");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public abstract int HorsePower { get; protected set; }
+
+        public double CubicCentimeters { get;}
+
+        public double CalculateRacePoints(int laps)
+        {
+            return this.CubicCentimeters / this.HorsePower * laps;
+        }
+    }
+}
---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Models/Motorcycles/PowerMotorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MXGP.Models.Motorcycles
+{
+    public class PowerMotorcycle : Motorcycle
+    {
+        private int horsePower;
+
+        public PowerMotorcycle(string model, int horsePower)
+            : base(model, horsePower, 450.0)
+        {
+        }
+
+        public override int HorsePower
+        {
+            get => this.horsePower;
+            protected set
+            {
+                if (value < 70 || value > 100)
+                {
+                    throw new ArgumentException($"Invalid horse power: {value}.");
+                }
+
+                this.horsePower = value;
+            }
+        }
+    }
+}
+
