9b10338a0015f200e1d0b6c4ed7d0654f2602292
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-09 11:29:40 +0200

    update

---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Models/Motorcycles/SpeedMotorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MXGP.Models.Motorcycles
+{
+    public class SpeedMotorcycle : Motorcycle
+    {
+        private int horsePower;
+
+        public SpeedMotorcycle(string model, int horsePower)
+            : base(model, horsePower, 125.0)
+        {
+        }
+
+        public override int HorsePower
+        {
+            get => this.horsePower;
+            protected set
+            {
+                if (value < 50 || value > 69)
+                {
+                    throw new ArgumentException($"Invalid horse power: {value}.");
+                }
+
+                this.horsePower = value;
+            }
+        }
+    }
+}
