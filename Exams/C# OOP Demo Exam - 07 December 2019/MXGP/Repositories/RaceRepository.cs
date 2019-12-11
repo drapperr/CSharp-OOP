73cf8e28d0d940100a2c49b83539b3083aa0400e
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-11 12:20:15 +0200

    update

---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Repositories/RaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using MXGP.Models.Races.Contracts;
+
+namespace MXGP.Repositories
+{
+    public class RaceRepository : Repository<IRace>
+    {
+        public override IRace GetByName(string name)
+        {
+            return this.models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
