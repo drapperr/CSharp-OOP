122a4ba80d08b03df459ade248d185dc88dfae49
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-11 12:20:02 +0200

    update

---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Repositories/MotorcycleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using MXGP.Models.Motorcycles.Contracts;
+
+namespace MXGP.Repositories
+{
+    public class MotorcycleRepository : Repository<IMotorcycle>
+    {
+        public override IMotorcycle GetByName(string name)
+        {
+            return this.models.FirstOrDefault(x => x.Model == name);
+        }
+    }
+}
