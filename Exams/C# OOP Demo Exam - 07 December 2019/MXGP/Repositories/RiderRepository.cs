7877b10b800f6c4fe173f5fe07d1fd3366e44329
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-12 10:11:06 +0200

    update

---
+++ Exams/C# OOP Demo Exam - 07 December 2019/MXGP/Repositories/RiderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using MXGP.Models.Riders.Contracts;
+
+namespace MXGP.Repositories
+{
+    public class RiderRepository : Repository<IRider>
+    {
+        public override IRider GetByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
