622601312f508313df4eccdf90cf3042a4a110a0
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-15 11:17:47 +0200

    update

---
+++ Exam15December/Exam/AquaShop/Core/Controller.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AquaShop.Core.Contracts;
+using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Repositories;
+using AquaShop.Repositories.Contracts;
+
+namespace AquaShop.Core
+{
+    public class Controller : IController
+    {
+        private readonly IRepository<IDecoration> decorationRepository;
+        private readonly List<IAquarium> aquariums;
+
+        public Controller()
+        {
+            this.decorationRepository = new DecorationRepository();
+            this.aquariums = new List<IAquarium>();
+        }
+
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            IAquarium aquarium = null;
+
+            if (aquariumType == "FreshwaterAquarium")
+            {
+                aquarium = new FreshwaterAquarium(aquariumName);
+            }
+            else if (aquariumType == "SaltwaterAquarium")
+            {
+                aquarium = new SaltwaterAquarium(aquariumName);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid aquarium type.");
+            }
+
+            this.aquariums.Add(aquarium);
+
+            return $"Successfully added {aquariumType}.";
+        }
+
+        public string AddDecoration(string decorationType)
+        {
+            IDecoration decoration = null;
+
+            if (decorationType == "Ornament")
+            {
+                decoration = new Ornament();
+            }
+            else if (decorationType == "Plant")
+            {
+                decoration = new Plant();
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid decoration type.");
+            }
+
+            this.decorationRepository.Add(decoration);
+
+            return $"Successfully added {decorationType}.";
+        }
+
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+            IDecoration decoration = this.decorationRepository.FindByType(decorationType);
+
+            if (decoration == null)
+            {
+                throw new InvalidOperationException($"There isn't a decoration of type {decorationType}.");
+            }
+
+            aquarium.AddDecoration(decoration);
+            this.decorationRepository.Remove(decoration);
+
+            return $"Successfully added {decorationType} to {aquariumName}.";
+        }//todo
+
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)//todo
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+            IFish fish = null;
+
+            if (fishType == "FreshwaterFish")
+            {
+                fish = new FreshwaterFish(fishName, fishSpecies, price);
+            }
+            else if (fishType == "SaltwaterFish")
+            {
+                fish = new SaltwaterFish(fishName, fishSpecies, price);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid fish type.");
+            }
+
+            if ((fish.GetType().Name.Contains("Freshwater") && aquarium.GetType().Name.Contains("Freshwater")) ||
+                (fish.GetType().Name.Contains("Saltwater") && aquarium.GetType().Name.Contains("Saltwater")))
+            {
+                aquarium.AddFish(fish);
+
+                return $"Successfully added {fishType} to {aquariumName}.";
+            }
+            else
+            {
+                return "Water not suitable.";
+            }
+        }
+
+        public string FeedFish(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            aquarium.Feed();
+
+            return $"Fish fed: {aquarium.Fish.Count}";
+        }
+
+        public string CalculateValue(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            decimal totalAquariumPrice = aquarium.Fish.Sum(f => f.Price) + aquarium.Decorations.Sum(d => d.Price);
+            return $"The value of Aquarium {aquariumName} is {totalAquariumPrice:F2}.";
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var aquarium in this.aquariums)
+            {
+                sb.AppendLine(aquarium.GetInfo());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
