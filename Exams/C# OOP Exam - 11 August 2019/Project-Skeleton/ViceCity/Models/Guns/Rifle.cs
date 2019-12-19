9eee0e064a56e76be769fb64a2dad3f9797b8b86
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-19 17:37:13 +0200

    update

---
+++ Exams/C# OOP Exam - 11 August 2019/Project-Skeleton/ViceCity/Models/Guns/Rifle.cs
@@ -0,0 +1,31 @@
+﻿namespace ViceCity.Models.Guns
+{
+    public class Rifle : Gun
+    {
+        private const int InitialBulletsPerBarrel = 50;
+        private const int InitialTotalBullets = 500;
+        private const int RifleShoot = 5;
+
+        public Rifle(string name)
+            : base(name, InitialBulletsPerBarrel, InitialTotalBullets)
+        {
+        }
+
+        public override int Fire()
+        {
+            if (!CanFire)
+            {
+                return 0;
+            }
+
+            if (this.BulletsPerBarrel == 0)
+            {
+                this.BulletsPerBarrel = InitialBulletsPerBarrel;
+                this.TotalBullets -= InitialBulletsPerBarrel;
+            }
+
+            this.BulletsPerBarrel -= RifleShoot;
+            return RifleShoot;
+        }
+    }
+}
