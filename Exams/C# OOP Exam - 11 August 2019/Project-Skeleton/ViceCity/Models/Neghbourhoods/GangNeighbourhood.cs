81e23d327ec04e24e1ff11141b2e92453c231785
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-19 17:37:50 +0200

    update

---
+++ Exams/C# OOP Exam - 11 August 2019/Project-Skeleton/ViceCity/Models/Neghbourhoods/GangNeighbourhood.cs
@@ -0,0 +1,35 @@
+﻿namespace ViceCity.Models.Neghbourhoods
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+    using ViceCity.Models.Players.Contracts;
+
+    public class GangNeighbourhood : INeighbourhood
+    {
+        public void Action(IPlayer mainPlayer, ICollection<IPlayer> civilPlayers)
+        {
+            foreach (var gun in mainPlayer.GunRepository.Models)
+            {
+                foreach (var civilPlayer in civilPlayers)
+                {
+                    while (civilPlayer.IsAlive && gun.CanFire)
+                    {
+                        civilPlayer.TakeLifePoints(gun.Fire());
+                    }
+                }
+            }
+
+            foreach (var player in civilPlayers.Where(p => p.IsAlive))
+            {
+                foreach (var gun in player.GunRepository.Models)
+                {
+                    while (gun.CanFire&& mainPlayer.IsAlive)
+                    {
+                        mainPlayer.TakeLifePoints(gun.Fire());
+                    }
+                }
+            }
+        }
+    }
+}
