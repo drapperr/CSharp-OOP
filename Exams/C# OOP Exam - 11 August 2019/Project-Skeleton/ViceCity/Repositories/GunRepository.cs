1b011f3180b8a0c0db28f1c1823eb893d88f9e36
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-20 18:47:45 +0200

    update

---
+++ Exams/C# OOP Exam - 11 August 2019/Project-Skeleton/ViceCity/Repositories/GunRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace ViceCity.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using ViceCity.Models.Guns.Contracts;
+    using Contracts;
+
+    public class GunRepository : IRepository<IGun>
+    {
+        private readonly List<IGun> models;
+
+        public GunRepository()
+        {
+            this.models = new List<IGun>();
+        }
+
+        public IReadOnlyCollection<IGun> Models => this.models.AsReadOnly();
+
+        public void Add(IGun model)
+        {
+            if (!this.models.Contains(model))
+            {
+                this.models.Add(model);
+            }
+        }
+
+        public bool Remove(IGun model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public IGun Find(string name)
+        {
+            return this.models.FirstOrDefault(m => m.Name == name);
+        }
+    }
+}
