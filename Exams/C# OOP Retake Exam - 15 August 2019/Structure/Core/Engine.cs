b3609e897f5c64757f4ba339e368fc66b1db9b99
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-23 11:22:50 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Core/Engine.cs
@@ -0,0 +1,76 @@
+﻿namespace SpaceStation.Core
+{
+    using System;
+    using System.Linq;
+
+    using Contracts;
+    using IO;
+    using SpaceStation.IO.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly IController controller;
+        private readonly IWriter writer;
+        private readonly IReader reader;
+
+        public Engine()
+        {
+            this.controller = new Controller();
+            ;
+            this.writer = new Writer();
+            this.reader = new Reader();
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                var input = reader.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    string result = string.Empty;
+
+                    if (input[0] == "AddAstronaut")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+
+                        result = controller.AddAstronaut(type, name);
+                    }
+                    else if (input[0] == "AddPlanet")
+                    {
+                        string name = input[1];
+                        string[] items = input.Skip(2).ToArray();
+
+                        result = controller.AddPlanet(name, items);
+                    }
+                    else if (input[0] == "RetireAstronaut")
+                    {
+                        string name = input[1];
+
+                        result = controller.RetireAstronaut(name);
+                    }
+                    else if (input[0] == "ExplorePlanet")
+                    {
+                        string name = input[1];
+
+                        result = controller.ExplorePlanet(name);
+                    }
+                    else if (input[0] == "Report")
+                    {
+                        result = controller.Report();
+                    }
+
+                    writer.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
