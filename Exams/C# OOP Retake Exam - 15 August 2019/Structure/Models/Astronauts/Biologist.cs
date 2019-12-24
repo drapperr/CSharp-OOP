c38857790c7b54d4cd191be7ff96560c9f50a319
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-24 16:35:12 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Models/Astronauts/Biologist.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    using System;
+
+    public class Biologist : Astronaut
+    {
+        private const int InitialOxygenPoints = 70;
+
+        public Biologist(string name) 
+            : base(name, InitialOxygenPoints)
+        {
+        }
+
+        public override void Breath()
+        {
+            this.Oxygen = Math.Max(this.Oxygen - 5, 0);
+        }
+    }
+}
