8560379b37be2cba4c62d0a6e953d7b46b89df2f
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-24 16:34:56 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,58 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    using System;
+
+    using Contracts;
+    using Bags;
+    using Utilities.Messages;
+
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+
+        protected Astronaut(string name, double oxygen)
+        {
+            this.Name = name;
+            this.Oxygen = oxygen;
+            this.Bag=new Backpack();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Name),ExceptionMessages.InvalidAstronautName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public double Oxygen
+        {
+            get => this.oxygen;
+            protected set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOxygen);
+                }
+
+                this.oxygen = value;
+            }
+        }
+
+        public bool CanBreath => this.Oxygen > 0;
+
+        public IBag Bag { get; }
+
+        public virtual void Breath()
+        {
+            this.Oxygen = Math.Max(this.Oxygen - 10, 0);
+        }
+    }
+}
