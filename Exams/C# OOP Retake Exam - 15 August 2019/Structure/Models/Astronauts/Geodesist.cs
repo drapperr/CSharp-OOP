f4aa70a381541a39ab23cc33e57da6b06a7704e5
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-24 16:35:52 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Models/Astronauts/Geodesist.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    public class Geodesist : Astronaut
+    {
+        private const int InitialOxygenPoints = 50;
+
+        public Geodesist(string name)
+            : base(name, InitialOxygenPoints)
+        {
+        }
+    }
+}
