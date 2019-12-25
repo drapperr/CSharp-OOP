31868ffe450d040e82582c2e553b6e2174c9d416
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-25 15:49:41 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Models/Mission/Mission.cs
@@ -0,0 +1,26 @@
+﻿namespace SpaceStation.Models.Mission
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Astronauts.Contracts;
+    using Planets;
+
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var astronaut in astronauts)
+            {
+                string item = planet.Items.FirstOrDefault();
+
+                while (item != null && astronaut.CanBreath)
+                {
+                    astronaut.Breath();
+                    astronaut.Bag.Items.Add(item);
+                    planet.Items.Remove(item);
+                    item = planet.Items.FirstOrDefault();
+                }
+            }
+        }
+    }
+}
