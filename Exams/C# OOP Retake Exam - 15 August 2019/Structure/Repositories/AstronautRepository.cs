3a6b2c1f1e3fe48a0fa30949130a2d6a6f73b0e7
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-26 11:31:38 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Repositories/AstronautRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace SpaceStation.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SpaceStation.Models.Astronauts.Contracts;
+    using Contracts;
+
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        private readonly List<IAstronaut> models;
+
+        public AstronautRepository()
+        {
+            this.models=new List<IAstronaut>();
+        }
+
+        public IReadOnlyCollection<IAstronaut> Models => this.models.AsReadOnly();
+
+        public void Add(IAstronaut model)
+        {
+            this.models.Add(model);
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+            return this.models.FirstOrDefault(a => a.Name == name);
+        }
+    }
+}
