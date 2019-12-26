33ab988af5e05d3793a0e88c3a4df07f6875521f
DraPpeRr <drapperr@users.noreply.github.com>
2019-12-26 11:32:18 +0200

    update

---
+++ Exams/C# OOP Retake Exam - 15 August 2019/Structure/Repositories/PlanetRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace SpaceStation.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Models.Planets;
+    using Contracts;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly List<IPlanet> models;
+
+        public PlanetRepository()
+        {
+            this.models = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => this.models.AsReadOnly();
+
+        public void Add(IPlanet model)
+        {
+            this.models.Add(model);
+        }
+
+        public bool Remove(IPlanet model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return this.models.FirstOrDefault(a => a.Name == name);
+        }
+    }
+}
