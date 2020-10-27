5b9efa2d6d2c8aafb7cae0387895d94d67283677
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-27 17:54:48 +0200

    update

---
+++ WebExams/SULS/Apps/BattleCards/Services/ICardService.cs
@@ -0,0 +1,20 @@
+﻿using BattleCards.ViewModels;
+using System.Collections.Generic;
+
+namespace BattleCards.Services
+{
+    public interface ICardService
+    {
+        public int Add(CardAddModel input);
+
+        public IEnumerable<CardViewModel> GetAll();
+
+        public void AddCardToCollection(string userId, int cardId);
+
+        public bool UserHasCard(string userId, int cardId);
+
+        public IEnumerable<CardViewModel> GetMyCollection(string userId);
+
+        public void RemoveCardFromCollection(string userId, int cardId);
+    }
+}
